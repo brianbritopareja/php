f7e20a6ebefae076a321a31548bbc2bf820785c0
brian brito pareja <bribripa@campusaula.com>
2023-10-18 13:01:22 +0200

    Signed-off-by: brian brito pareja <bribripa@campusaula.com>

--- php-intro/index.php
+++ php-intro/index.php
@@ -11,6 +11,38 @@
     <?php
     //Holaa
     echo 'Hola Caracola';
+
+    //Puedes añadir etiquetas como texto
+
+    echo '<br>';
+
+    echo '<h1>Linea con h1 desde PHP</h1>';
+
+    echo '<br>';
+
+    ?>
+
+    <h2>otro bloque de</h2>
+
+    <?php
+
+    $nombre='Brian';
+
+    $edad='19';
+
+   
+
+    //echo del contenido de la variable
+
+    echo $nombre;
+
+ 
+
+    //concatenacion de variables
+
+    echo '<h3></h3>'
+
+
     ?>
 
 
