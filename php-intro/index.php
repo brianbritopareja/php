a808399c0441e52812bf2646497315ca5bc5125f
brian brito pareja <bribripa@campusaula.com>
2023-10-18 12:51:59 +0200

    Signed-off-by: brian brito pareja <bribripa@campusaula.com>

--- php-intro/index.php
+++ php-intro/index.php
@@ -1,1 +1,20 @@
-Hola
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Uso PHP</title>
+</head>
+<body>
+    <H1>Primer uso de PHP</H1>
+    
+    <?php
+    //Holaa
+    echo 'Hola Caracola';
+    echo 'Hola Caracola';
+    echo 'Hola Caracola';
+    ?>
+
+
+</body>
+</html>
