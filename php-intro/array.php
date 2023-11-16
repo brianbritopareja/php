353d193212d3c1d0cbc14372a56924c6177b67b1
brianbritopareja <147610114+brianbritopareja@users.noreply.github.com>
2023-11-16 12:48:23 +0100

    Add files via upload

--- php-intro/array.php
+++ php-intro/array.php
@@ -6,8 +6,8 @@
     <title>Arrays</title>
 </head>
 <body>
-    <?php
-    //Arrays indexados
+    <?php  
+    //Arrays indexados 
     $colores = array("rojo", "verde", "azul");
     echo $colores[0], '<br>';
     echo $colores[1], '<br>';
@@ -15,14 +15,14 @@
     // echo $colores[3], '<br>';
     
 
-    //Arrays asociativos
+    //Arrays asociativos 
     $edades = array("Ana"=>25, "Luis"=>35, "Pepe"=>23);
     echo $edades["Ana"] . '<br>';
     echo $edades["Luis"] . '<br>';
     echo $edades["Pepe"] . '<br>';
 
 
-    //Array multidimensional
+    //Array multidimensional 
     $matriz = array(
         array(1,2,3), 
         array(4,5,6), 
@@ -38,7 +38,7 @@
         $frutas=array("melón", "pera", "platano");
         echo count($frutas) . '<br>';
     
-        //Array multidimensional asociativo
+        //Array multidimensional asociativo 
         $animales = array(
             "mamiferos"=>array("perro","gato","vaca"),
             "reptiles"=>array("serpiente","lagarto","rana")
@@ -50,7 +50,7 @@
         $animal = $animales["mamiferos"][1];
         echo "$animal <br>";
 
-        //para recorrer un array con for
+        //para recorrer un array con for 
         $frutas=array("melón", "pera", "platano");
         $longitud = count($frutas);
 
@@ -58,13 +58,13 @@
             echo "$frutas[$i] <br>";
         }
 
-        //Foreach
+        //Foreach para recorrer un array
         $numeros = array(1,2,3,4,5);
         foreach($numeros as $n){
             echo "El número es: $n <br>";
         }
 
-        //array asociativo
+        //array asociativo con foreach
         $colores=array("rojo"=>"#FF0000", "verde"=>"#00FF00","azul"=>"#0000FF");
         foreach($colores as $color=>$hexadecimal){
             echo "El color $color tiene un codigo $hexadecimal <br>";
